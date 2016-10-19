4c8d8440d0b9d60188ee90ffbbc3d02f811ab111
elhendricks <elhendricks@gmail.com>
2016-10-19 10:20:44 -0700

    set up  dev environment and wrote fizzbuzz

---
+++ test/fizzbuzz.test.js
@@ -0,0 +1,26 @@
+var fizzBuzz = require('../lib/fizzbuzz');
+
+var assert = require('assert');
+
+describe('fizzbuzz', function(){
+    it('should return an array of length of input', function() {
+        assert.deepEqual(fizzBuzz(16), [
+            1, 
+            2, 
+            'fizz',     // 3
+            4, 
+            'buzz',     // 5
+            'fizz',     // 6
+            7, 
+            8, 
+            'fizz',     // 9
+            'buzz',     // 10
+            11,
+            'fizz',     // 12
+            13,
+            14,
+            'fizzbuzz', // 15
+            16
+        ]); 
+    });
+});
