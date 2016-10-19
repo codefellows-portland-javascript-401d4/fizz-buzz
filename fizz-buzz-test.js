ccd7d84223efacba449e633d8d479495f2b3c711
dratech <alfonsotech@nym.hush.com>
2016-10-19 14:41:34 -0700

    added chai library

--- fizz-buzz-test.js
+++ fizz-buzz-test.js
@@ -1,9 +1,9 @@
 'use strict';
 var assert = require('chai').assert;
-var fizzbuzz = require('fizz-buzz.js');
+var fizzbuzz = require('./fizz-buzz.js');
 
 describe('testing fizzbuzz functionality', function() {
-    it('prints out fizz, buzz, fizzbuzz, or the number', function(){
+    it('prints out Fizz, Buzz, FizzBuzz, or the number', function(){
         assert.deepEqual(fizzbuzz(5), [1, 2, "Fizz", 4, "Buzz"]);
     });
 });
