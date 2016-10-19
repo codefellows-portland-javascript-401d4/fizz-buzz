2faa62a52cb57fd58c5c33306ce5c7e583d970b8
dratech <alfonsotech@nym.hush.com>
2016-10-19 14:08:24 -0700

    added fizzbuzz.js and test file and node modules to test

--- fizz-buzz-test.js
+++ fizz-buzz-test.js
@@ -1,1 +1,9 @@
-Const assert = 
+'use strict';
+var assert = require('mocha').assert;
+var fizzbuzz = require('./fizzbuzz');
+
+describe('fizzbuzz', function() {
+    it('prints out fizz, buzz, fizzbuzz, or the number', function(){
+        assert.deepEqual(fizzbuzz(15), ['fizz', 'buzz']);
+    });
+});
