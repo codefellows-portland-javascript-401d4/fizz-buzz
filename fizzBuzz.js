c48e16268313f564d9b9c8637b7977299a607a1d
Michelle Sri <michelle.sri@gmail.com>
2016-10-20 14:10:10 -0700

    finished fizzbuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -2,18 +2,18 @@ function fizzBuzz(num){
   let fizzBuzzArr = [];
 
   if(typeof(num) === 'number'){
-    for(var i = 0; i < num; i++){
+    for(var i = 1; i <= num; i++){
       if(i % 3 === 0 && i % 5 === 0){
-        console.log('fizzbuzz');
+        // console.log('fizzbuzz');
         fizzBuzzArr.push('fizzbuzz');
       } else if(i % 3 === 0){
-        console.log('fizz');
+        // console.log('fizz');
         fizzBuzzArr.push('fizz');
       } else if(i % 5 === 0){
-        console.log('buzz');
+        // console.log('buzz');
         fizzBuzzArr.push('buzz');
       } else {
-        console.log('this number is not divisible by 3 or 5');
+        // console.log('this number is not divisible by 3 or 5');
         fizzBuzzArr.push(i);
       }
       // console.log(fizzBuzzArr);
